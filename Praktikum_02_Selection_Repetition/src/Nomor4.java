8d7ae15370b09c97595d589f746f499af243831e
ErdianaRagilSyawala <113496521+ErdianaRagilSyawala@users.noreply.github.com>
2022-10-09 19:28:26 +0700

    Add files via upload

---
+++ Praktikum_02_Selection_Repetition/src/Nomor4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Nomor4 {
+
+	public static void main(String[] args) {
+	@SuppressWarnings("unused")
+	int hasil = 1, i;
+
+	@SuppressWarnings("resource")
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Masukan nilai faktorial : ");
+	int angka = input.nextInt();
+
+
+	if(angka < 20) {
+		System.out.println("Nilai kurang dari 20!!");
+	}else {
+		System.out.print(" Faktor dari "+angka+" Adalah"+" = ");
+		for(i=angka;i>=1;i--) {
+
+			String k = (angka%i==0)? i+", ":"" ;
+			System.out.print(k);
+		}
+	}
+
+	}
+}
