e63e33820d88001381c948ce9987e786c7c7852f
ErdianaRagilSyawala <113496521+ErdianaRagilSyawala@users.noreply.github.com>
2022-11-18 23:58:12 +0700

    Add files via upload

---
+++ Praktikum_4_Array/src/Latihan2.java
@@ -0,0 +1,10 @@
+public class Latihan2 {
+	public static void main(String[] args) {
+		int [] arr = new int[5];
+		
+		for (int i  = 0; i < arr.length; i++) {
+			arr[i] = i * 5;
+			System.out.println("Array ke - " + i + " = " + arr[i]);
+		}
+	}
+}
